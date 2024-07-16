0e53091b6ffb9690f3254e719d5a9ee4f3102046
privatslabs <privatslabs@outlook.fr>
2024-07-16 22:30:22 +0000

    menu bien integrer

---
+++ components/menuVue.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="menu">
+
+    <v-icon @click="close">mdi-close</v-icon>
+    <br>
+
+    <v-list class="box">
+      <img
+        src="https://lh3.googleusercontent.com/jqf4IU7510s8OYtkVCjBaVjku1DsruhZtn9LGla7PMbmflRr4B9Mls2pskmfMLoUicTTcdTlaAa-wD6BbNbveAbzxpHN6I1h7A"
+        width="50"
+      />
+      <span>Telephones</span>
+    </v-list>
+    <div class="box">
+      <img
+        src="https://lh3.googleusercontent.com/gb1-ZHkdOS6IO9LnFMV7liSbgbt0D5Y2c9z626Ds7phkHx0a08xrHHFiDnmY1IlGs4tj9qh0VQmPdu3REi3An2JuoJcn_sCgNfs"
+        width="50"
+      />
+      <span>Ecouteur</span>
+    </div>
+    <div class="box">
+      <img
+        src="/article/montre.webp"
+        width="50"
+      />
+      <span>Montres</span>
+    </div>
+    <div class="box">
+      <img
+        src="https://lh3.googleusercontent.com/gDXehJ9Qf611YfA4gj_GTEmdbxKlKq8uCWqp8spYkFiNQNdE1DKHoq8G3UAAll_1HpZ0DiiLn-jMKy6ea44vy-NGCzKd3U5zUIA"
+        width="50"
+
+      />
+      <span>Accessoires</span>
+    </div>
+    <div class="box">
+      <img
+        src="https://lh3.googleusercontent.com/YN24eOCoidong7T3E1foOb7nn6irWVnKprK7TdXKooClNrLKih6GLszuAurv36z4glpTLsWZOwFYFqRtIS4flbacfA3boUMvGY8=rw-e365-nu-w601"
+        width="50"
+      />
+      <span>Tablettes</span>
+    </div>
+
+    <br />
+    <v-spacer></v-spacer>
+    <v-divider></v-divider>
+    <br />
+
+    <div class="box">
+        <v-icon>mdi-bag-checked</v-icon>
+        <span>Commandes</span>
+    </div>
+    <div class="box">
+        <v-icon>mdi-help-circle</v-icon>
+        <span>Aide</span>
+    </div>
+
+ 
+    <br> <br> <br>
+</div>
+</template>
+
+<style scoped>
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  z-index: 20;
+  left: 0%;
+  top: 55px;
+  width: 0%;
+  height: 100%;
+  background-color: white;
+  padding: 0px;
+  margin-bottom: 220px;
+  transition: 0.3s;
+  animation: ease-in-out;
+}
+
+.menu .box{
+    display: none;
+    align-items: center;
+    margin-bottom: 20px;
+}
+.box img{
+    margin-right: 15px;
+}
+.mdi-close{
+    margin-left: auto;
+    display: none;
+}
+
+.mdi-bag-checked, .mdi-help-circle{
+    margin-right: 20px;
+}
+
+</style>
+
+<script>
+
+export default ({
+    data() {
+        return{
+
+        }
+        
+    },
+    methods: {
+    close() {
+      document.querySelector(".menu").style.width = "0%";
+      document.querySelector(".menu").style.padding = "0px";
+      document.querySelector(".mdi-close").style.display = "none";
+
+      const boxes = document.querySelectorAll(".box");
+      boxes.forEach((box) => {
+        box.style.display = "none";
+      });
+    },
+  },
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,65 +1,82 @@
 <template>
   <v-app>
     <div>
+      <v-toolbar elevation="0">
+        <v-app-bar-nav-icon @click="menuO"></v-app-bar-nav-icon>
 
-        <v-toolbar elevation="0">
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Market</v-toolbar-title>
 
-          <v-toolbar-title>Market</v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <div class="d-flex left">
+          <v-icon>mdi-magnify</v-icon>
 
           <v-spacer></v-spacer>
 
-          <div class="d-flex left">
+          <v-icon>mdi-cart</v-icon>
 
-            <v-icon>mdi-magnify</v-icon>
+          <v-spacer></v-spacer>
 
-            <v-spacer></v-spacer>
+          <v-icon>mdi-account-circle</v-icon>
+        </div>
+      </v-toolbar>
+    </div>
+    <br />
+    <br />
 
-            <v-icon>mdi-cart</v-icon>
+    <menu-vue />
 
-            <v-spacer></v-spacer>
-
-            <v-icon>mdi-account-circle</v-icon>
-            
-          </div>
-
-        </v-toolbar>
-      </div>
-      <br> <br>
-
-
-
-        <Nuxt />
-     
+    <Nuxt />
   </v-app>
 </template>
 
+
+<script>
+import menuVueVue from "../components/menuVue.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    menuO() {
+      document.querySelector(".menu").style.width = "95%";
+      document.querySelector(".menu").style.padding = "20px";
+      document.querySelector(".mdi-close").style.display = "block";
+
+      const boxes = document.querySelectorAll(".box");
+      boxes.forEach((box) => {
+        box.style.display = "flex";
+      });
+    },
+  },
+};
+</script>
+
+
+
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
-@import url('https://fonts.googleapis.com/css2?family=Rubik+Glitch+Pop&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Rubik+Glitch+Pop&display=swap");
 
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-   font-family: "Poppins", sans-serif;
+  font-family: "Poppins", sans-serif;
   font-style: normal;
 }
 
 body {
-     font-family: "Poppins", sans-serif;
+  font-family: "Poppins", sans-serif;
   font-style: normal;
-    font-size: 14px;
-    line-height: 1.29;
-    margin-top: 75px
+  font-size: 14px;
+  line-height: 1.29;
+  margin-top: 75px;
 }
 
-.left{
-  width: 100px;
-}
-
-.v-toolbar{
+.v-toolbar {
   position: fixed;
   z-index: 20;
   top: 0;
@@ -67,14 +84,16 @@ body {
   width: 100%;
 }
 
-.v-toolbar__title{
+.v-toolbar__title {
   color: yellowgreen;
   font-family: "Rubik Glitch Pop", system-ui;
   font-weight: 900;
   font-style: normal;
 }
 
-
+.left {
+  width: 100px;
+}
 </style>
 
 
