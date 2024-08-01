5e5d0d53b05adf3ac4eb582c019c3c51140b6657
privatslabs <privatslabs@outlook.fr>
2024-08-01 23:53:41 +0000

    updated

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,9 @@ module.exports = {
     ],
     plugins: [
       '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator'
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods'
     ]
   }
   
